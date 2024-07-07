4ca4695449dae0b3fc63ff25553bdbb16b1d1017
Aristotelis <aristotelis.demetriou@gmail.com>
2024-07-07 16:00:24 +0200

    Finished GANs

---
+++ GAN/craft_gif.py
@@ -0,0 +1,42 @@
+import imageio
+import os
+import argparse
+import re
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Create GIF from images')
+    parser.add_argument('--image_folder', type=str, required=True, help='Directory containing the .bmp images')
+    parser.add_argument('--output_file', type=str, required=True, help='Output GIF file')
+    parser.add_argument('--duration', type=float, default=0.25, help='Duration of each frame in the GIF')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    # Directory containing the .bmp images
+    image_folder = args.image_folder
+
+    # Output GIF file
+    output_file = args.output_file
+
+    # Regular expression to extract the number
+    def extract_number(file_name):
+        match = re.search(r'epoch_(\d+)', file_name)
+        if match:
+            return int(match.group(1))
+        else:
+            return float('inf')  # Place any non-matching file names at the end
+
+    # Read images from the directory, sorted by the extracted number
+    images = []
+    for file_name in sorted(os.listdir(image_folder), key=extract_number):
+        if file_name.startswith('epoch_') and file_name.endswith('.bmp'):
+            print(file_name)
+            file_path = os.path.join(image_folder, file_name)
+            images.append(imageio.imread(file_path))
+
+    # Create the GIF
+    print('Creating GIF...')
+    imageio.mimsave(output_file, images, duration=args.duration)
+    print('GIF created at', output_file)
--- GAN/model.py
+++ GAN/model.py
@@ -194,6 +194,271 @@ class Generator_64_RGB(nn.Module):
                     nn.init.constant_(m.bias, val=0)
 
 
+class Discriminator_128_G(nn.Module):
+    def __init__(self) -> None:
+        super(Discriminator_128_G, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 1 x 128 x 128
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=True),
+            nn.LeakyReLU(0.2, True),
+            # State size. 64 x 64 x 64
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, True),
+            # State size. 128 x 32 x 32
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, True),
+            # State size. 256 x 16 x 16
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True),
+            # State size. 512 x 8 x 8
+            #################################################  New Layers  #################################################
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True),
+            ################################################################################################################
+            # State size. 512 x 4 x 4
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0), bias=True),
+            nn.Sigmoid()
+        )
+
+        # self.conv1 = nn.Conv2d(3, 64, (4, 4), (2, 2), (1, 1), bias=True)
+        # self.conv2 = nn.Conv2d(64, 128, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn2 = nn.BatchNorm2d(128)
+        # self.conv3 = nn.Conv2d(128, 256, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn3 = nn.BatchNorm2d(256)
+        # self.conv4 = nn.Conv2d(256, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn4 = nn.BatchNorm2d(512)
+        # self.conv5 = nn.Conv2d(512, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn5 = nn.BatchNorm2d(512)
+        # self.conv6 = nn.Conv2d(512, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn6 = nn.BatchNorm2d(512)
+        # self.conv7 = nn.Conv2d(512, 1, (4, 4), (1, 1), (0, 0), bias=True)
+        # self.lr = nn.LeakyReLU(0.2, True)
+        # self.sigmoid = nn.Sigmoid()
+        
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+        # print(f"Discriminator_512_RGB: {out.size()}")
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.conv1(x))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn2(self.conv2(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn3(self.conv3(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn4(self.conv4(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn5(self.conv5(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn6(self.conv6(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.sigmoid(self.conv7(x))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        out = torch.flatten(out, 1)
+
+        return out
+
+class Generator_128_G(nn.Module):
+    def __init__(self) -> None:
+        super(Generator_256_G, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 100, going into a convolution.
+            nn.ConvTranspose2d(in_channels=100, out_channels=512, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 4 x 4
+            #################################################  New Layers  #################################################
+            nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 8 x 8
+            ################################################################################################################
+            nn.ConvTranspose2d(512, 256, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # state size. 256 x 16 x 16
+            nn.ConvTranspose2d(256, 128, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # state size. 128 x 32 x 32
+            nn.ConvTranspose2d(128, 64, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. 64 x 64 x 64
+            nn.ConvTranspose2d(64, 1, (4, 4), (2, 2), (1, 1), bias=True),
+            nn.Tanh()
+            # state size. 1 x 128 x 128
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+    # Support PyTorch.script function.
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+
+        return out
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, 0.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, 1.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, mean=0.0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, val=0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, mean=1.0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, val=0)
+
+
+class Discriminator_128_RGB(nn.Module):
+    def __init__(self) -> None:
+        super(Discriminator_128_RGB, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 3 x 128 x 128
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=True),
+            nn.LeakyReLU(0.2, True),
+            # State size. 64 x 64 x 64
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, True),
+            # State size. 128 x 32 x 32
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, True),
+            # State size. 256 x 16 x 16
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True),
+            # State size. 512 x 8 x 8
+            #################################################  New Layers  #################################################
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True),
+            ################################################################################################################
+            # State size. 512 x 4 x 4
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0), bias=True),
+            nn.Sigmoid()
+        )
+
+        # self.conv1 = nn.Conv2d(3, 64, (4, 4), (2, 2), (1, 1), bias=True)
+        # self.conv2 = nn.Conv2d(64, 128, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn2 = nn.BatchNorm2d(128)
+        # self.conv3 = nn.Conv2d(128, 256, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn3 = nn.BatchNorm2d(256)
+        # self.conv4 = nn.Conv2d(256, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn4 = nn.BatchNorm2d(512)
+        # self.conv5 = nn.Conv2d(512, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn5 = nn.BatchNorm2d(512)
+        # self.conv6 = nn.Conv2d(512, 512, (4, 4), (2, 2), (1, 1), bias=False)
+        # self.bn6 = nn.BatchNorm2d(512)
+        # self.conv7 = nn.Conv2d(512, 1, (4, 4), (1, 1), (0, 0), bias=True)
+        # self.lr = nn.LeakyReLU(0.2, True)
+        # self.sigmoid = nn.Sigmoid()
+        
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+        # print(f"Discriminator_512_RGB: {out.size()}")
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.conv1(x))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn2(self.conv2(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn3(self.conv3(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn4(self.conv4(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn5(self.conv5(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.lr(self.bn6(self.conv6(x)))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        # x = self.sigmoid(self.conv7(x))
+        # print(f"Discriminator_512_RGB: {x.size()}")
+        out = torch.flatten(out, 1)
+
+        return out
+
+class Generator_128_RGB(nn.Module):
+    def __init__(self) -> None:
+        super(Generator_128_RGB, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 100, going into a convolution.
+            nn.ConvTranspose2d(in_channels=100, out_channels=512, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 4 x 4
+            #################################################  New Layers  #################################################
+            nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 8 x 8
+            ################################################################################################################
+            nn.ConvTranspose2d(512, 256, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # state size. 256 x 16 x 16
+            nn.ConvTranspose2d(256, 128, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # state size. 128 x 32 x 32
+            nn.ConvTranspose2d(128, 64, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. 64 x 64 x 64
+            nn.ConvTranspose2d(64, 3, (4, 4), (2, 2), (1, 1), bias=True),
+            nn.Tanh()
+            # state size. 3 x 128 x 128
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+    # Support PyTorch.script function.
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+
+        return out
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, 0.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, 1.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, mean=0.0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, val=0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, mean=1.0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, val=0)
+
+
+
 class Discriminator_256_G(nn.Module):
     def __init__(self) -> None:
         super(Discriminator_256_G, self).__init__()
