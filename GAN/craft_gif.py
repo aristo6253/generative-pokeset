43bd4e8515f8a34afaea9b27f7a8b1be019f50e9
Aristotelis <aristotelis.demetriou@gmail.com>
2024-07-07 16:11:48 +0200

    add gif

--- GAN/craft_gif.py
+++ GAN/craft_gif.py
@@ -8,6 +8,7 @@ def parse_args():
     parser.add_argument('--image_folder', type=str, required=True, help='Directory containing the .bmp images')
     parser.add_argument('--output_file', type=str, required=True, help='Output GIF file')
     parser.add_argument('--duration', type=float, default=0.25, help='Duration of each frame in the GIF')
+    parser.add_argument('--up_to', type=int, default=None, help='Number of images to include in the GIF')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -30,8 +31,8 @@ if __name__ == '__main__':
 
     # Read images from the directory, sorted by the extracted number
     images = []
-    for file_name in sorted(os.listdir(image_folder), key=extract_number):
-        if file_name.startswith('epoch_') and file_name.endswith('.bmp'):
+    for i, file_name in enumerate(sorted(os.listdir(image_folder), key=extract_number)):
+        if file_name.startswith('epoch_') and file_name.endswith('.bmp') and (args.up_to is None or i + 1 <= args.up_to):
             print(file_name)
             file_path = os.path.join(image_folder, file_name)
             images.append(imageio.imread(file_path))
